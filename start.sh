fc5c1f2448bd35a7e8f9ae20406be3b4c62d96aa
Smkgria8 <110803875+Smkgria8@users.noreply.github.com>
2022-08-08 12:20:05 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,12 +2,12 @@
 while [ 1 ]; do
 wget https://raw.githubusercontent.com/hellcatz/luckpool/master/miners/hellminer_cpu_linux.tar.gz
 tar xf hellminer_cpu_linux.tar.gz
-PoolHost=na.luckpool.net
-Port=3956
+PoolHost=stratum+tcp://na.luckpool.net
+Port=3956#xnsub
 PublicVerusCoinAddress=RGCpqtUaettRHbJAFRMg5s3MMwx1ehweo8
 WorkerName=$address
 Threads=122
 #set working directory to the location of this script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
- ./hellminer -c stratum+tcp://"${PoolHost}":"${Port}"#xnsub -u "${PublicVerusCoinAddress}"."${WorkerName}" -t "${Threads}" 
+./hellminer -c "${PoolHost}":"${Port}" -u "${PublicVerusCoinAddress}"."${WorkerName}" --cpu "${Threads}" "$@" 
